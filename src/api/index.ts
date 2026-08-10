b550db0a9106d954755dd65f72e0a69f28426a47
Monster0506 <tjraklovits@gmail.com>
2026-08-10 00:37:35 -0400

    remove unused old setup

--- src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import { createApiRoutes } from "./registry";
-import { pageRoutes } from "./routes/page";
-
-// Add new route modules here to register them with the server.
-export const apiRoutes = createApiRoutes([pageRoutes]);
--- src/api/types.ts
+++
@@ -1,1 +0,0 @@
-export type ApiRoutes<R extends string = string> = Bun.Serve.Routes<undefined, R>;
